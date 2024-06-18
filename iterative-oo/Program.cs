2d3957db38ff65bec14e46ceace67451d9fe3e26
rcmilan <13418519+rcmilan@users.noreply.github.com>
2024-06-18 09:24:57 -0300

    Section 3: Begin

--- iterative-oo/Program.cs
+++ iterative-oo/Program.cs
@@ -1,15 +1,1 @@
-﻿using iterative_oo;
-
-while (true)
-{
-    var input = InputStatement.Read();
-
-    if (input == InputStatement.Empty)
-        break;
-
-    var solution = SolutionExpression.Solve(input);
-
-    Console.WriteLine(solution?.ToString() ?? "No Solution!");
-}
-
-Console.WriteLine("Halt!");
+﻿Console.WriteLine("Halt!")
